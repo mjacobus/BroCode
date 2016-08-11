32b6cd319a8a7a1e64701852f77bcb67986cba23
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-08-11 11:35:24 +0200

    Added test that does not really test, only to get it going

---
+++ tests/CsFixerFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BroTest\Code;
+
+use Bro\Code\CsFixerFactory;
+use PHPUnit_Framework_TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\CS\Console\Application;
+
+class CsFixerFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CsFixerFactory
+     */
+    protected $object;
+
+    /**
+     * @before
+     */
+    public function initialize()
+    {
+        $this->object = new CsFixerFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function createRetrunsAnConsoleApplication()
+    {
+        $product = $this->object->create([
+            'binFilePath' => __FILE__,
+            'preset'      => 'bro',
+        ]);
+
+        $this->assertInstanceOf(Application::class, $product);
+    }
+}
